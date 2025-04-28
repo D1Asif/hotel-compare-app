cb5e31871e0e08aa0e519187b6ad7252d5225f98
Dewan Asifur Rahman <dasifrahman@gmail.com>
2025-04-28 11:56:36 +0600

    browser throttling fixed

--- backend/app/scraper/spiders/booking_spider.py
+++ backend/app/scraper/spiders/booking_spider.py
@@ -76,7 +76,9 @@ class BookingSpider(scrapy.Spider):
             self.start_url += f'%3Bclass%3D{self.star_rating}'
 
         options = Options()
-        # options.add_argument("--headless")
+        options.add_argument("--disable-background-timer-throttling")
+        options.add_argument("--disable-backgrounding-occluded-windows")
+        options.add_argument("--disable-renderer-backgrounding")
         self.driver = webdriver.Chrome(options=options)
 
     def start_requests(self):
